4754a880b10c6a7939bfa8bc67dea082ef463f4e
Cheri <avyishi@gmail.com>
2015-06-18 20:06:45 -0700

    Added favoriting feature to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   mount_uploader :image, ImageUploader
 
@@ -20,5 +21,9 @@ class User < ActiveRecord::Base
 
   def voted(post)
     votes.where(post_id: post.id).first
+  end
+  
+  def favorited(post)
+    favorites.where(post_id: post.id).first
   end
 end
