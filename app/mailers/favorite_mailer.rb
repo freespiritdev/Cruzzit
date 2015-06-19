712670d43b55c2fb4ae92083f6b19e3365bc837c
Cheri <avyishi@gmail.com>
2015-06-19 12:17:41 -0700

    Added mailer and formatted outgoing email

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,17 @@
+class FavoriteMailer < ApplicationMailer
+  default from: "admin@cruzzit.com"
+
+  def new_comment(user, post, comment)
+ 
+     # New Headers
+     headers["Message-ID"] = "<comments/#{comment.id}@cruzzit.com>"
+     headers["In-Reply-To"] = "<post/#{post.id}@cruzzit.com>"
+     headers["References"] = "<post/#{post.id}@cruzzit.com>"
+ 
+     @user = user
+     @post = post
+     @comment = comment
+ 
+     mail(to: user.email, subject: "New comment on #{post.title}")
+   end
+end
