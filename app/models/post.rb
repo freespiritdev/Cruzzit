bfb82c4bb79b88c9c5b06edf7a74b98d28c04c08
Cheri <avyishi@gmail.com>
2015-06-30 08:53:48 -0700

    Lowered character length for new posts title and body

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,8 @@ class Post < ActiveRecord::Base
   default_scope { order('rank DESC') }
   scope :visible_to, ->(user) { user ? all : joins(:topic).where('topics.public' => true) }
 
-  validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
+  validates :title, length: { minimum: 3 }, presence: true
+  validates :body, length: { minimum: 10 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
 
